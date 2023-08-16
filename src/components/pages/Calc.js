ae1cb67b5279041a14fce67fcd7601279893e62c
terra <terra@gmail.com>
2023-08-16 16:45:36 +0800

    dockerfile

--- src/components/pages/Calc.js
+++ src/components/pages/Calc.js
@@ -1,156 +1,80 @@
-// import React, { useState } from "react"
-// import { BrowserRouter as Router } from 'react-router-dom'
-// import { Route } from 'react-router-dom'
-// import { Routes } from 'react-router-dom'
-
-// // import Home from './components/pages/Home'
-// // import Calc from './components/pages/Calc'
-// // import Register from './components/pages/Register'
-
-// // import './App.css'
-
-// import TextInput from "./components/TextInput"
-// import Button from "./components/Button"
-
-// const App = () => {
-//   const [weight, setWeight] = useState();
-//   const [height, setHeight] = useState();
-//   const [bmi, setBmi] = useState();
-//   const [bmiClass, setBmiClass] = useState();
-
-//   const handleHeightChange = (event) => setHeight(event.target.value);
-//   const handleWeightChange = (event) => setWeight(event.target.value);
-
-//   const computeBmi = () => {
-//     let bmiValue = (weight / (height / 100) ** 2).toFixed(2);
-//     setBmi(bmiValue);
-//     let bmiClass = getBmi(bmiValue);
-//     setBmiClass(bmiClass);
-//     setHeight("")
-//     setWeight("")
-//   };
-
-//   const getBmi = (bmi) => {
-    
-//     if (bmi < 18.5) {
-//       return "Underweight";
-//     }
-//     if (bmi >= 18.5 && bmi < 24.9) {
-//       return "Normal weight";
-//     }
-//     if (bmi >= 25 && bmi < 29.9) {
-//       return "Overweight";
-//     }
-//     if (bmi >= 30) {
-//       return "Obesity";
-//     }
-//   };
-
-//   return (
-//     <div className="App">
-//     <div className="container">
-//       <div
-//         style={{
-//           display: "block",
-//           width: "50%",
-//           margin: "0 auto",
-//           padding: "20px",
-//           boxSizing: "border-box",
-//         }}
-//       >
-//         <h2>Welcome to our BMI Calculator!</h2> <br></br>
-//         <h3>Please enter your IC number</h3>
-//       </div>
-//       <div className="row">
-//         <TextInput
-//           label="IC Number"
-//           placeholder="Enter IC Number without symbol"
-//           handleChange={handleHeightChange}
-//           value={number}
-//         />
-//       </div>
-        
-//       </div>
-//       <div className="row">
-//         <Button label="SEARCH" onClick={computeBmi} />
-//       </div>
-//       <div className="row">
-//         {
-//           isNaN(bmi)?null:<h3>Your BMI = {bmi}</h3> 
-//         }
-        
-//       </div>
-//       <div className="row">
-//         <h3>{bmiClass}</h3>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import '../../App.css'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../App.css';
+import axios from 'axios';
   
 export default function BmiCalculator() {
-    const [heightValue, setHeightValue] = useState('');
-    const [weightValue, setWeightValue] = useState('');
-    const [bmiValue, setBmiValue] = useState('');
-    const [bmiMessage, setBmiMessage] = useState('');
-  
-    const calculateBmi = () => {
-        if (heightValue && weightValue) {
-            const heightInMeters = heightValue / 100;
-            const bmi = (weightValue / (heightInMeters * heightInMeters)).toFixed(2);
-            setBmiValue(bmi);
-  
-            let message = '';
-            if (bmi < 18.5) {
-                message = 'You are Underweight';
-            } else if (bmi >= 18.5 && bmi < 25) {
-                message = 'You are Normal weight';
-            } else if (bmi >= 25 && bmi < 30) {
-                message = 'You are Overweight';
-            } else {
-                message = 'You are Obese';
+
+    const [nric, setSearchNRIC] = useState('');
+    const [bmi, setSearchBMI] = useState(null);
+    //const [error, setError] = useState(null);
+    //const [newnric, setNewNRIC] = useState('nric');
+
+    const handleChange = (event) => {
+
+        setSearchNRIC(event.target.value);
+
+    }
+
+    const api = axios.create({
+        baseURL: 'https://24ec-202-186-167-246.ngrok-free.app/', // Replace with your base API URL
+        method:"GET",
+        headers: {
+            'Content-Type': 'application/json',
+            
+            },
+        withCredentials: true, // Include credentials if needed (cookies, etc.)
+      });
+
+    const handleSubmit = async () => {
+
+            try {
+                const response = await api.options(`/getBmi/${nric}`);
+                // Assuming the API returns the age in the response data
+                setSearchBMI(response.data.bmi);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setSearchBMI(null); // Clear age in case of an error
             }
-            setBmiMessage(message);
-        } else {
-            setBmiValue('');
-            setBmiMessage('');
-        }
     };
-  
+
+
+
     return (
       <div className="text-center m-5-auto">
             <h2>Bmi Calculator</h2>
             <h5>Check Your BMI</h5>
-            <form onSubmit= "/calc">
-                <p>
-                    <label>I/C Number</label><br/>
-                    <input type="text" name="ic" required />
-                </p>
-                <p>
-                <button className="calculate-btn" onClick={calculateBmi}>Click to Calculate BMI</button>
-                </p>
+            <form>
+            <p>
+                <label>I/C Number</label><br/>
+                <input 
+                    type="text" 
+                    name={nric}
+                    onChange={handleChange} />
+            </p>
 
-              {bmiValue && bmiMessage && (
-                <div className="result">
-                    <p>
-                        Your BMI: <span className="bmi-value">{bmiValue}</span>
-                    </p>
-                    <p>
-                        Result: <span className="bmi-message">{bmiMessage}</span>
-                    </p>
-                </div>
-              )}
+            <div className="footer">
+                <input type= "button" onClick={()=>handleSubmit()} name="btn" value="Calculate"/>
+            </div>
 
             </form>
+
+            {bmi !== null ? (
+                <div className="result">
+                <p>
+                    Your BMI: {bmi}
+                </p>
+                </div>
+            ) : (
+                <div className="result">
+                <p>No data available for the IC {nric}.</p>
+                </div>
+            )}
             <footer>
                 <p><Link to="/">Back to Homepage</Link>.</p>
             </footer>
         </div>
     );
 }
+
+//<button onClick={()=>handleSubmit()} type="submit" className="btn">Calculate</button>
--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,43 +1,99 @@
-import React from 'react'
+import React, {useState,setState} from 'react';
+import './style.css'
 import { Link } from 'react-router-dom'
-
-import '../../App.css'
+import axios from 'axios'
 
 export default function RegisterPage() {
+    
+    const [username, setUsername] = useState(null);
+    const [nric, setNRIC] = useState(null);
+    const [age, setAge] = useState(null);
+    const [height,setHeight] = useState(null);
+    const [weight,setWeight] = useState(null);
 
-    return (
-        <div className="text-center m-5-auto">
-            <h2>Register</h2>
-            <h5>Fill in your information</h5>
-            <form onSubmit= "/register">
-                <p>
-                    <label>Username</label><br/>
-                    <input type="text" name="name" required />
-                </p>
-                <p>
-                    <label>I/C Number</label><br/>
-                    <input type="text" name="ic" required />
-                </p>
-                <p>
-                    <label>Age</label><br/>
-                    <input type="text" name="age" required />
-                </p>
-                <p>
-                    <label>Weight</label><br/>
-                    <input type="text" name="weight" required />
-                </p>
-                <p>
-                    <label>Height</label><br/>
-                    <input type="text" name="height" required />
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Register</button>
-                </p>
-            </form>
+    const handleInputChange = (e) => {
+        const {id , value} = e.target;
+        if(id === "username"){
+            setUsername(value);
+        }
+        if(id === "nric"){
+            setNRIC(value);
+        }
+        if(id === "age"){
+            setAge(value);
+        }
+        if(id === "height"){
+            setHeight(value);
+        }
+        if(id === "weight"){
+            setWeight(value);
+        }
+
+    }
+
+    const handleSubmit  = () => {
+        const userInformation = {
+            username: username,
+            nric: nric,
+            age: age,
+            height: height,
+            weight: weight,
+          };
+          
+          console.log(userInformation);
+
+          const url = 'https://24ec-202-186-167-246.ngrok-free.app/add_user';
+
+          axios.post(url, userInformation)
+            .then((response) => {
+            // Handle the response from the Flask server
+            console.log(response.data);
+            })
+            .catch((error) => {
+            // Handle errors
+            console.error(error);
+            });
+    };
+
+    return(
+        <div className="form">
+            <div className="form-body">
+                <div className="username">
+                    <label className="form__label" for="username"> Username </label>
+                    <input className="form__input" type="text" value={username} onChange = {(e) => handleInputChange(e)} id="username" placeholder="username"/>
+                </div>
+                <div className="nric">
+                    <label className="form__label" for="nric">IC Number </label>
+                    <input  type="text" name="" id="nric" value={nric}  className="form__input" onChange = {(e) => handleInputChange(e)} placeholder="nric"/>
+                </div>
+                <div className="age">
+                    <label className="form__label" for="age">Age </label>
+                    <input  
+                    type="text" 
+                    id="age" 
+                    className="form__input" 
+                    value={age} 
+                    onChange = {(e) => handleInputChange(e)} 
+                    placeholder="age"/>
+                </div>
+                <div className="height">
+                    <label className="form__label" for="height">Height </label>
+                    <input className="form__input" type="height"  id="height" value={height} onChange = {(e) => handleInputChange(e)} placeholder="height"/>
+                </div>
+                <div className="weight">
+                    <label className="form__label" for="weight">Weight </label>
+                    <input className="form__input" type="weight" id="weight" value={weight} onChange = {(e) => handleInputChange(e)} placeholder="weight"/>
+                </div>
+            </div>
+            <div class="footer">
+                <button onClick={()=>handleSubmit()} type="submit" class="btn">Register</button>
+            </div>
+
             <footer>
                 <p><Link to="/">Back to Homepage</Link>.</p>
             </footer>
-        </div>
-    )
 
+        </div>
+       
+    )       
 }
